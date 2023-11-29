bee1653158b20b93724da2db63c9cae2e98f95ce
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2023-11-28 23:46:44 -0500

    v14 + recurring reminders
    updated to work with discordjs v14, also recurring reminders work now

--- commands/scrim-scheduler.js
+++ commands/scrim-scheduler.js
@@ -3,17 +3,16 @@ const chrono = require('chrono-node');
 const scheduledScrimSchema = require('../models/scheduled-scrim-schema');
 const db = require('../models/db');
 const config = require('../config.json');
-const { Client, Intents } = require('discord.js');
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
 const schedule = require('node-schedule');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel] });
 
 // Function to send a reminder message in the channel
 async function sendChannelReminder(scheduledScrim) {
     const channel = await client.channels.fetch(scheduledScrim.channel);
-    //console.log('Fetched channel:', channel.id); // Log the channel ID for debugging
     const role = scheduledScrim.role;
-    // Send the reminder message in the channel with user ping
+    // Send the reminder message in the channel with role ping
     await channel.send(`<@&${role}>: ${scheduledScrim.content}`);
 }
 
@@ -25,12 +24,20 @@ client.login(config.token);
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('scrim-scheduler')
-        .setDescription('Schedule a scrim with an automatic reminder!')
-        .addChannelOption(option => option.setName('channel').setDescription('The channel the reminder will be sent to').setRequired(true))
-        .addStringOption(option => option.setName('date').setDescription('The date the reminder will be sent').setRequired(true))
-        .addStringOption(option => option.setName('message').setDescription('The message that will be included').setRequired(true))
-        .addRoleOption(option => option.setName('role').setDescription('The role to ping for the reminder').setRequired(true)
+    .setName('scrim-scheduler')
+    .setDescription('Schedule a scrim with an automatic reminder!')
+    .addChannelOption(option => option.setName('channel').setDescription('The channel the reminder will be sent to').setRequired(true))
+    .addStringOption(option => option.setName('date').setDescription('The date the reminder will be sent').setRequired(true))
+    .addStringOption(option => option.setName('message').setDescription('The message that will be included').setRequired(true))
+    .addRoleOption(option => option.setName('role').setDescription('The role to ping for the reminder').setRequired(true))
+    .addStringOption(option => option.setName('recurrence').setDescription('Choose the recurrence').setRequired(true)
+        .addChoices(
+            {name: 'Daily',value: 'daily'},
+            {name: 'Weekly', value:'weekly'},
+            {name: 'Monthly', value: 'monthly'},
+            {name:'None', value:'none'},
+            {name:'Test', value:'test'}
+        )
     ),
 
     async execute(interaction) {
@@ -40,29 +47,41 @@ module.exports = {
         const date = chrono.parseDate(interaction.options.getString('date'));
         const message = interaction.options.getString('message');
         const role = interaction.options.getRole('role');
+        const recurrence = interaction.options.getString('recurrence');
 
         try {
-            const newScrim = new scheduledScrimSchema({
+            let newScrim;
+
+            newScrim = new scheduledScrimSchema({
                 date: new Date(date),
                 content: message,
                 channel: channel.id,
                 role: role.id,
+                recurrence: recurrence,
             });
 
             await newScrim.save();
-            //console.log('Scrim saved successfully:', newScrim);
 
             // Schedule the reminder for this scrim
             const now = new Date();
-            if (new Date(date) > now) {
-                schedule.scheduleJob(new Date(date), () => {
-                    sendChannelReminder(newScrim);
-                });
-            }
+            schedule.scheduleJob(new Date(date), async () => {
+                sendChannelReminder(newScrim);
+
+                if (recurrence == 'none') {
+                    // Remove the non-recurring reminder from the database after sending
+                    await scheduledScrimSchema.findByIdAndDelete(newScrim._id);
+                } else {
+                    // Update the date for recurring reminders
+                    updateDateBasedOnRecurrence(newScrim, recurrence);
+                    await newScrim.save();
+                    // Schedule the recursive function for the next occurrence
+                    scheduleRecurringJob(newScrim, recurrence);
+                }
+            });
 
             // Send a confirmation DM to the user who wrote the command
             interaction.user.send(`You have a scrim scheduled: "${message}" in ${channel} on ${date}`);
-            // The little only you can see this thing that the bot will send
+            // The little 'only you can see this' thing that the bot will send
             return interaction.reply({ content: `Scheduled "${message}" in ${channel} on ${date}`, ephemeral: true });
 
         } catch (error) {
@@ -72,3 +91,33 @@ module.exports = {
 
     },
 };
+
+// Recursive function to handle scheduling for recurring reminders
+function scheduleRecurringJob(scrim, recurrence) {
+    schedule.scheduleJob(scrim.date, async () => {
+        sendChannelReminder(scrim);
+        updateDateBasedOnRecurrence(scrim, recurrence);
+        await scrim.save();
+        scheduleRecurringJob(scrim, recurrence);
+    });
+}
+
+function updateDateBasedOnRecurrence(scrim, recurrence) {
+    // Update the date based on the recurrence type
+    switch (recurrence) {
+        case 'daily':
+            scrim.date.setDate(scrim.date.getDate() + 1);
+            break;
+        case 'weekly':
+            scrim.date.setDate(scrim.date.getDate() + 7);
+            break;
+        case 'monthly':
+            scrim.date.setMonth(scrim.date.getMonth() + 1);
+            break;
+        case 'test':// Will recur every 5 min just to test
+            scrim.date.setMinutes(scrim.date.getMinutes() + 5);
+            break;
+        default:
+            break;
+    }
+}
