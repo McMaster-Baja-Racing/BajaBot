614e51a76adf5beb0e2508e4b34f8484953ce634
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2024-01-05 16:28:10 -0500

    using google api

--- commands/sheetsdb.js
+++ commands/sheetsdb.js
@@ -1,17 +1,16 @@
+const { google } = require('googleapis');
 const { SlashCommandBuilder } = require('discord.js');
-const axios = require('axios');
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('sheetdb')
-        .setDescription('sheet db for todo'),
+        .setDescription('Google Sheets for todo'),
 
     async execute(interaction) {
         try {
             await interaction.reply({ content: "Fetching data from the spreadsheet...", ephemeral: true });
 
-            // Define the thread IDs for each sheet
             const threadIds = {
                 'Chassis': '1192481865056137247',
                 'Controls': '1192508568927219773', 
@@ -20,7 +19,14 @@ module.exports = {
                 'DAQ': '1192706895614582885',
             };
 
-            // Array to store embeds for each tab
+            const auth = await google.auth.getClient({
+                keyFile: './credentials/baja-to-do-bot-fef8bb884c17.json',
+                scopes: 'https://www.googleapis.com/auth/spreadsheets',
+            });
+
+            const sheets = google.sheets({ version: 'v4', auth });
+            const spreadsheetId = '1pb2W0BvAOMFeM4AXIbLzxM0dWJGYtqago8_8J4S5wEI';
+
             const responseEmbeds = [];
 
             const sheetColorMap = {
@@ -32,8 +38,17 @@ module.exports = {
             };
 
             for (const sheetName of Object.keys(threadIds)) {
-                const response = await axios.get(`https://sheetdb.io/api/v1/trx2ey55em789?sheet=${sheetName}`);
-
+                const range = sheetName;
+            
+                const response = await sheets.spreadsheets.values.get({
+                    spreadsheetId,
+                    range,
+                });
+            
+                const sheetData = response.data.values.slice(1);
+            
+                console.log(sheetData);
+            
                 const currentDate = new Date();
                 currentDate.setHours(0, 0, 0, 0);
                 const thisWeekStartDate = new Date(currentDate);
@@ -41,60 +56,42 @@ module.exports = {
                 thisWeekEndDate.setDate(currentDate.getDate() + (7 - currentDate.getDay()));
                 const nextWeekStartDate = new Date(thisWeekEndDate);
                 nextWeekStartDate.setDate(thisWeekEndDate.getDate() + 1);
-
-                const sheetData = response.data.filter(row => {
+            
+                const nextWeekEndDate = new Date(nextWeekStartDate);
+                nextWeekEndDate.setDate(nextWeekStartDate.getDate() + 7);
+            
+                const sheetRows = sheetData.map(row => ({
+                    'Task': row[0],
+                    'Start Date': row[1],
+                    'Due Date': row[2],
+                    'Status': row[3],
+                    'Assigned To': row[4],
+                    'Notes': row[5],
+                }));
+                console.log(sheetRows);
+            
+                const sheetColor = sheetColorMap[sheetName];
+            
+                const thisWeekTasks = sheetRows.filter(row => {
                     const dueDate = new Date(row['Due Date']);
-                    return (dueDate >= thisWeekStartDate && dueDate <= thisWeekEndDate) || (dueDate >= nextWeekStartDate && dueDate < new Date(nextWeekStartDate.getTime() + 7 * 24 * 60 * 60 * 1000));
+                    return (dueDate >= thisWeekStartDate && dueDate <= thisWeekEndDate);
                 });
-
-                const thisWeekTasks = sheetData.filter(row => {
+            
+                const nextWeekTasks = sheetRows.filter(row => {
                     const dueDate = new Date(row['Due Date']);
-                    return dueDate >= thisWeekStartDate && dueDate <= thisWeekEndDate;
+                    return (dueDate >= nextWeekStartDate && dueDate < nextWeekEndDate);
                 });
+            
+                console.log(thisWeekStartDate, thisWeekEndDate, nextWeekStartDate, nextWeekEndDate);
 
-                const nextWeekTasks = sheetData.filter(row => {
-                    const dueDate = new Date(row['Due Date']);
-                    return dueDate >= nextWeekStartDate && dueDate < new Date(nextWeekStartDate.getTime() + 7 * 24 * 60 * 60 * 1000);
-                });
 
-                const formatTasks = (tasks, includeDetails = true) => {
-                    const embed = new EmbedBuilder()
-                        .setColor(sheetColorMap[sheetName])
-                        .setURL('https://docs.google.com/spreadsheets/d/1pb2W0BvAOMFeM4AXIbLzxM0dWJGYtqago8_8J4S5wEI/edit#gid=0')
-                        .setAuthor({ name: 'To-Do Reminder', iconURL: 'https://i.imgur.com/ALT9CPo.jpg' })
-
-                    if (includeDetails) {
-                        embed.setTitle(`${sheetName}: This Week`)
-                            .addFields(
-                                tasks.map(row => ({
-                                    name: `__**${row.Task}**__`,
-                                    value: `> **Due Date:** ${row['Due Date']}\n> **Status:** ${row.Status}\n> **Assigned To:** ${row['Assigned To']}\n> **Notes:** ${row.Notes}`,
-                                }))
-                            )
-                            .setFooter({ text: 'Try to get these tasks done this week!' });
-                    } else {
-                        embed.setTitle(`${sheetName}: Next Week`)
-                            .addFields(
-                                tasks.map(row => ({
-                                    name: '\u200B',
-                                    value: `     ${row.Task}     `,
-                                    inline: true
-                                }))
-                            )
-                            .setFooter({ text: 'Prepare to get these tasks done next week!' });
-                    }
-
-                    return embed;
-                };
-
-                const thisWeekFormattedData = formatTasks(thisWeekTasks);
-                const nextWeekFormattedData = formatTasks(nextWeekTasks, false);
+                const thisWeekFormattedData = formatTasks(thisWeekTasks, sheetName, sheetColor, 'This Week');
+                const nextWeekFormattedData = formatTasks(nextWeekTasks, sheetName, sheetColor, 'Next Week');
 
                 responseEmbeds.push({ sheetName, formattedData: thisWeekFormattedData });
                 responseEmbeds.push({ sheetName, formattedData: nextWeekFormattedData });
             }
 
-            // Send each embed to the corresponding thread
             for (const { sheetName, formattedData } of responseEmbeds) {
                 const threadId = threadIds[sheetName];
                 if (threadId) {
@@ -109,7 +106,24 @@ module.exports = {
                 }
             }
         } catch (error) {
-            console.error('Error fetching data from SheetDB:', error);
+            console.error('Error fetching data from Google Sheets:', error);
         }
     }
 };
+
+function formatTasks(tasks, sheetName, sheetColor, timeFrame) {
+    const embed = new EmbedBuilder()
+        .setColor(sheetColor)
+        .setURL('https://docs.google.com/spreadsheets/d/1pb2W0BvAOMFeM4AXIbLzxM0dWJGYtqago8_8J4S5wEI/edit#gid=490843265')
+        .setAuthor({ name: 'To-Do Reminder', iconURL: 'https://i.imgur.com/ALT9CPo.jpg' })
+        .setTitle(`${sheetName}: ${timeFrame}`)
+        .addFields(
+            tasks.map(row => ({
+                name: `__**${row.Task}**__`,
+                value: `> **Start Date:** ${row['Start Date']}\n> **Due Date:** ${row['Due Date']}\n> **Status:** ${row.Status}\n> **Assigned To:** ${row['Assigned To']}\n> **Notes:** ${row.Notes}`,
+            }))
+        )
+        .setFooter({ text: `Try to get these tasks done ${timeFrame.toLowerCase()}!` });
+
+    return embed;
+}
