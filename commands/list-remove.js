398af5eb50df2c434c738e6ba87939e6800d3418
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2024-06-21 17:19:10 -0400

    fixed remove

--- commands/list-remove.js
+++ commands/list-remove.js
@@ -3,11 +3,11 @@ const newListModule = require('./list-newlist.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('view')
-        .setDescription('View details of a specific item on the to-do list')
+        .setName('remove')
+        .setDescription('Removes an item from the to-do list by its number')
         .addIntegerOption(option =>
             option.setName('number')
-                .setDescription('The number of the item to view')
+                .setDescription('The number of the item to remove')
                 .setRequired(true)
         ),
         
@@ -21,17 +21,20 @@ module.exports = {
             return interaction.reply({ content: 'Invalid item number.', ephemeral: true });
         }
 
-        const item = list[number - 1];
+        list.splice(number - 1, 1);
 
         const embed = new EmbedBuilder()
-            .setTitle(`Item ${number}: ${item.item}`)
-            .setDescription(item.description)
+            .setTitle('To Do List')
+            .setDescription(list.length > 0 ? list.map((entry, index) => `${index + 1}. ${entry.item}`).join('\n') : 'This is your to-do list')
             .setColor(0x1e2124);
 
-        if (item.assignee) {
-            embed.addFields({ name: 'Assignee', value: item.assignee, inline: true });
-        }
+        const channel = interaction.channel;
 
-        await interaction.reply({ embeds: [embed] });
+        const messageId = newListModule.getMessageId(channelId);
+
+        const message = await channel.messages.fetch(messageId);
+        await message.edit({ embeds: [embed] });
+
+        await interaction.reply({ content: 'Item removed from the list!', ephemeral: true });
     },
 };
